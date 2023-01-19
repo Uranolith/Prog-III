6dfcfcf8f3516b9019b43279cfd29a86d244567a
Florian Wittmann <floriansebas.wittm@outlook.com>
2023-01-19 18:04:37 +0100

    Uebung 2.3

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,18 @@
 #include <iostream>
 
 int main() {
+    int *x = new int;                                       // x ist adresse     * -> value  //  & -> adresse
+    int *y = new int;                                       // y ist adresse     * -> value  //  & -> adresse
 
-    //const type name = value;		    Konstante vom Typ type
-    //type * const name = value;		konstanter Zeiger auf eine Variable vom Typ type
-    //const type *name = value;		    (variabler) Zeiger auf eine Konstante vom Typ type
-    //const type * const name = value;	konstanter Zeiger auf eine Konstante vom Typ type
+    std::cout << "x adresse: " << x << "\t";
+    std::cout << "y adresse: " << y << std::endl;
 
-
-    int i;                              //  Variable i vom Typ int
-    int *ip;                            //  (variabler) Zeiger ip auf eine Variable vom Typ int
-    int *const cp = &i;                 //  Definition eines konstanten Zeigers cp auf eine Variable vom Typ int
-    const int ci = 7;                   //  Definition einer Konstanten ci vom Typ int mit dem Wert 7
-    const int *cip;                     //  (variabler) Zeiger cip auf ein Konstante vom Typ int
-    const int *const cicp = &ci;        //  Definition eines konstanten Zeigers cicp auf eine konstante vom Typ int
+    swapi(*x, *y);                                          // übergabe VALUE
 
 
-            i = ci;                     //  TRUE
-         // ci = 8;                     //  FALSE                    
-            *cp = ci;                   //  TRUE
-         // cp = &ci;                   //  FALSE
-            cip = &ci;                  //  TRUE
-            cip = cicp;                 //  TRUE
-         // *cip = 7;                   //  FALSE
-         // ip = cip;                   //  FALSE
-         
+    std::cout << "x adresse: " << x << "\t";
+    std::cout << "y adresse: " << y << std::endl;
+
     return 0;
 }
 
